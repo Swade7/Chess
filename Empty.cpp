02b33ec22f9b64834cbdc6b671f38874c88cc2e5
Swade7 <swade7@harding.edu>
2023-05-31 15:22:50 -0500

    Created the Empty class

---
+++ Empty.cpp
@@ -0,0 +1,21 @@
+# pragma once
+
+#include "Empty.hpp"
+
+// Constructor
+Empty::Empty()
+	: Pieces(PieceType::Empty, Player::None)
+{}
+
+// Destructor
+Empty::~Empty() = default;
+
+bool Empty::checkValidMove(const Move& move, const Chess& chess) const
+{
+	return false;
+}
+
+void Empty::movePiece(const Move& move, Chess& chess)
+{
+	// Add output telling user that the location is empty
+}
---
+++ Empty.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Pieces.hpp"
+
+class Empty : public Pieces
+{
+public:
+	// Constructor
+	Empty();
+
+	// Destructor
+	~Empty() override;
+
+	bool checkValidMove(const Move& move, const Chess& chess) const override;
+	void movePiece(const Move& move, Chess& chess) override;
+};
